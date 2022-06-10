4b89c575776e72660ea3abfdfc5d2525340452fd
e277 <ezramuir12@gmail.com>
2022-06-10 10:37:26 -0500

    display list of heroes and display details

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 25, name: 'Dr. Nice' },
+  { id: 26, name: 'Narcos' },
+  { id: 27, name: 'Elastic-man' },
+  { id: 28, name: 'Magneto' },
+  { id: 29, name: 'Stiles' },
+  { id: 30, name: 'Tornado' },
+  { id: 31, name: 'Wonder-Woman' },
+  { id: 32, name: 'Fire-Storm' },
+  { id: 33, name: 'Mr. Geek' },
+  { id: 34, name: 'She-Hulk' },
+];
